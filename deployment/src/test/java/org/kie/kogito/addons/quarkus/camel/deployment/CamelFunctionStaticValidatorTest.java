3ce027956c821de6904bb6b88f18da728fb2848f
Ricardo Zanini <zanini@redhat.com>
2022-12-08 13:24:32 -0300

    enforce args as expression
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- deployment/src/test/java/org/kie/kogito/addons/quarkus/camel/deployment/CamelFunctionStaticValidatorTest.java
+++ deployment/src/test/java/org/kie/kogito/addons/quarkus/camel/deployment/CamelFunctionStaticValidatorTest.java
@@ -17,7 +17,12 @@ import static org.kie.kogito.quarkus.serverless.workflow.WorkflowCodeGenUtils.ge
 public class CamelFunctionStaticValidatorTest {
 
     @ParameterizedTest
-    @CsvSource({ "camel-soap.sw.json,callSoap", "newsletter-subscription.sw.json,subscribeToNewsletter", "camel-soap-no-args.sw.json,callSoap" })
+    @CsvSource({
+            "camel-soap.sw.json,callSoap",
+            "newsletter-subscription.sw.json,subscribeToNewsletter",
+            "camel-soap-no-args.sw.json,callSoap",
+            "camel-soap-expression-args.sw.json,callSoap"
+    })
     public void verifyValidWorkflowsWithCamelFunctions(final String workflowFile, final String functionDefName) throws URISyntaxException {
         final Stream<Workflow> workflows = getWorkflows(Stream.of(
                 Paths.get(requireNonNull(CamelFunctionStaticValidatorTest.class.getResource("/" + workflowFile)).toURI())));
